d579228c2ff4a79b57491eb22cee41cff0865645
IA-llan <afbrasil0@gmai.com>
2024-07-17 02:12:39 -0300

    Finalziada aula de tipagem, iniciada aula de listas

--- Aula0-variaveis.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  int idade = 23;
-  double altura = 1.73;
-  bool booleando = (idade == altura);
-  print(idade);
-  print(altura);
-  print(booleando);
-}
---
+++ Aula_DART/aula1-Listas.dart
@@ -0,0 +1,7 @@
+void main(){
+  // Necessário passar o tipo qua vai popular a lista
+  List<String> nomes = ['Allan', 'Carlo', 'Guru', 'Guiri', 'Phedo'];
+
+  print(nomes[2]);
+  print(nomes.length);
+}
